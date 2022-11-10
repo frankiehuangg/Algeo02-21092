b7614a6e816e0d517194dea49f585c893e7b41b1
Genvictus <johannstarkmk42@gmail.com>
2022-11-10 22:25:44 +0700

    Fix index access bug

--- src/EigenFunction/eigenfunction.py
+++ src/EigenFunction/eigenfunction.py
@@ -18,23 +18,23 @@ def polyKofaktor(M):
     else:
         minorMat = [[0 for i in range(dim-1)] for i in range(dim-1)]
         polyEq = 0
-        for i in range(M[0]):
+        for i in range(dim):
             # Copy matriks minor
-            for j in range(dim):
+            for j in range(1, dim):
                 for k in range(i):
-                    minorMat[j][k] = M[j+1][k]
+                    minorMat[j-1][k] = M[j][k]
 
-            for j in range(dim):
+            for j in range(1, dim):
                 for k in range(i+1, dim):
-                    minorMat[j][k-1] = M[j+1][k]
+                    minorMat[j-1][k-1] = M[j][k]
 
             # hitung polynomial
             polyTmp = poly.polymul(M[0][i], polyKofaktor(minorMat))
             if(i%2 == 0):
                 polyTmp = [(-x) for x in polyTmp]
             polyEq = poly.polyadd(polyEq, polyTmp)
-            
-    return polyEq
+
+        return polyEq
 
 
 def eigenValue(A):
