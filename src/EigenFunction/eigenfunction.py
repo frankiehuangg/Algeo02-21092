0c59738623e25d5ecd591929ffd48502441200f7
Genvictus <johannstarkmk42@gmail.com>
2022-11-12 01:24:03 +0700

    Create method eigenVectors, modify value rounding

--- src/EigenFunction/eigenfunction.py
+++ src/EigenFunction/eigenfunction.py
@@ -36,6 +36,7 @@ def polyKofaktor(M):
             polyEq = poly.polyadd(polyEq, polyTmp)
 
         return polyEq
+    #
 
 
 def eigenValue(A):
@@ -47,6 +48,7 @@ def eigenValue(A):
     # eigMat : polynomial[][]
     # charPol : polynomial
     # i : int
+    # roots : float[]
 
     # ALGORITMA
     eigMat = [[El for El in ROW] for ROW in A]  # buat matriks A - λI
@@ -54,4 +56,116 @@ def eigenValue(A):
         eigMat[i][i] = (eigMat[i][i], -1)
     
     charPol = polyKofaktor(eigMat)
-    return poly.polyroots(charPol)
+    roots =  poly.polyroots(charPol)
+    # round to nearest 16
+    roots = np.round_(roots, decimals = 16)
+    roots = [*set(roots)]
+    return roots
+
+
+
+def eigenVectors(A, eigVal):
+    # A adalah sebuah matriks persegi
+    # mengembalikan vektor-vektor basis eigen A
+    # dari nilai eigen eigVal
+
+    # KAMUS LOKAL
+    # M, lmdI, ret : float[][]
+    # R : (float[][], int[])    # value hasil method sp.rref()
+    # n, i, j, t : int
+    # r, c, rpivot : int
+    # prec = int
+
+
+    # ALGORITMA
+    dim = len(A)
+    lmdI = np.identity(dim) * eigVal
+    M = [[El for El in ROW] for ROW in A]
+    M = np.array(M)
+    M = M - lmdI
+    M = np.round_(M, decimals = 16)
+
+    prec = 16
+    while True:
+        # Eliminasi Gauss-Jordan
+        Mtemp = [[El for El in ROW] for ROW in M]
+        r=0; c=0
+        while(r < dim and c < dim):
+            rpivot = r
+            while(rpivot < dim-1 and Mtemp[rpivot][c] == 0):
+                rpivot = rpivot + 1
+            # rpivot = dim-1 or m[rpivot][c] != 0
+            if(Mtemp[rpivot][c] != 0):
+                # tukar baris dengan pivot
+                if(r != rpivot):
+                    temp = Mtemp[r]
+                    Mtemp[r] = Mtemp[rpivot]
+                    Mtemp[rpivot] = temp
+                
+                # bagi baris pivot dengan elemen bukan 0 pertama
+                Mtemp[r] = Mtemp[r]/Mtemp[r][c]
+
+                # eliminasi turun
+                for i in range(r+1,dim):
+                    Mtemp[i] = Mtemp[i] - Mtemp[r]*Mtemp[i][c]
+                
+                # eliminasi naik
+                for i in range(r):
+                    Mtemp[i] = Mtemp[i] - Mtemp[r]*Mtemp[i][c]
+
+                
+                Mtemp = np.round_(Mtemp, decimals = prec)
+                r = r+1
+            c = c+1
+        # r == dim or c == dim
+        
+        if (r != dim):
+            break
+        else:
+            prec = prec - 1
+        
+    n = dim-r   # jumlah vektor basis adalah dimensi matriks - jumlah baris bukan 0
+    ret = [[0 for i in range(dim)] for j in range(n)]
+    t = 0
+    for i in range(dim):
+        if(Mtemp[i][i] == 0):
+            ret[t] = [-Mtemp[j][i] for j in range(dim)]
+            ret[t][i] = 1
+
+    return ret
+    #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# EIGENVALUE CALCULATIONS BY THE BOOK!!!
+# + NO QR DECOMPOSITION!!!
+# + NAIVE IMPLEMENTATION POSSIBLE WITH BASIC KNOWLEDGE OF EIGEN VALUE!!!
+# + IKUT KELAS ALGEO 100% WORK NO HACK REQUIRED!!!
+# - require built-in polynomial function to solve polynomial roots
+# - computationally expensive
